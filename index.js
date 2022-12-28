cf1c51260d6c28588ec001075f810801f190f44d
Ahnaf-dev <ahnafbari99@gmail.com>
2022-12-28 14:46:03 -0700

    finishing animation with web API

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
 function setColorCycleAnim(red, blue, green, increment) {
   let body = document.querySelector("body");
-  // let rgb = `rgb(${red}, ${green}, ${blue})`;
 
   const colorCycle = [
     {
@@ -50,9 +49,105 @@ function setColorCycleAnim(red, blue, green, increment) {
         4
       )}, ${incrementRGB(blue, increment, 4)})`,
     },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 5)}, ${incrementRGB(
+        green,
+        increment,
+        5
+      )}, ${incrementRGB(blue, increment, 5)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 6)}, ${incrementRGB(
+        green,
+        increment,
+        6
+      )}, ${incrementRGB(blue, increment, 6)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 7)}, ${incrementRGB(
+        green,
+        increment,
+        7
+      )}, ${incrementRGB(blue, increment, 7)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 8)}, ${incrementRGB(
+        green,
+        increment,
+        8
+      )}, ${incrementRGB(blue, increment, 8)})`,
+    },
+
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 8)}, ${incrementRGB(
+        green,
+        increment,
+        8
+      )}, ${incrementRGB(blue, increment, 8)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 7)}, ${incrementRGB(
+        green,
+        increment,
+        7
+      )}, ${incrementRGB(blue, increment, 7)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 6)}, ${incrementRGB(
+        green,
+        increment,
+        6
+      )}, ${incrementRGB(blue, increment, 6)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 5)}, ${incrementRGB(
+        green,
+        increment,
+        5
+      )}, ${incrementRGB(blue, increment, 5)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 4)}, ${incrementRGB(
+        green,
+        increment,
+        4
+      )}, ${incrementRGB(blue, increment, 4)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 3)}, ${incrementRGB(
+        green,
+        increment,
+        3
+      )}, ${incrementRGB(blue, increment, 3)})`,
+    },
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 2)}, ${incrementRGB(
+        green,
+        increment,
+        2
+      )}, ${incrementRGB(blue, increment, 2)})`,
+    },
+
+    {
+      backgroundColor: `rgb(${incrementRGB(red, increment, 1)}, ${incrementRGB(
+        green,
+        increment,
+        1
+      )}, ${incrementRGB(blue, increment, 1)})`,
+    },
+    {
+      backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+    },
   ];
 
-  console.log(colorCycle);
+  const colorCycleTiming = {
+    duration: 4000,
+    easing: "ease-in",
+    fill: "forwards",
+    iterations: Infinity,
+  };
+
+  body.animate(colorCycle, colorCycleTiming);
 }
 
 function incrementRGB(value, inc, multiply) {
